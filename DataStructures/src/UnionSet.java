ce71be97eb9814e4a2ba80f1a61369a3cc9a101a
Erwan Demairy <erwan.demairy@laposte.net>
2013-06-03 15:16:58 +0200

    Modification of union-find structure, to keep trakc of the size of the sets.

--- DataStructures/src/UnionSet.java
+++ DataStructures/src/UnionSet.java
@@ -9,10 +9,13 @@
  */
 public class UnionSet {
     protected int[] pset;
+    protected int[] size;
     public UnionSet(int n) {
         pset = new int[n];
+        size = new int[n];
         for (int i = 0; i < n; i++) {
             pset[i] = i;
+            size[i] = 1;
         }
     }
     
@@ -29,7 +32,20 @@ public class UnionSet {
         return findSet(i) == findSet(j);
     }
     
+    int getSize(int i) {
+        return size[findSet(i)];
+    }
+    
+    int[] getSizes() {
+        return size;
+    }
+    
     void unionSet(int i, int j) {
-        pset[findSet(i)] = findSet(j);
+        int fsi = findSet(i);
+        int fsj = findSet(j);
+        pset[fsi] = fsj;
+        if (fsi != fsj) {
+            size[fsj] += size[fsi];
+        }
     }
 }
--- DataStructures/test/UnionSetTest.java
+++ DataStructures/test/UnionSetTest.java
@@ -17,37 +17,44 @@ public class UnionSetTest {
         UnionSet u = new UnionSet(5);
         assertEquals(0, u.pset[0]);
         assertEquals(1, u.pset[1]);
-        assertEquals(2, u.pset[2]);        
+        assertEquals(2, u.pset[2]);
         assertEquals(3, u.pset[3]);
         assertEquals(4, u.pset[4]);
-        
+        assertArrayEquals(new int[]{1, 1, 1, 1, 1}, u.getSizes());
+
         u.unionSet(0, 1);
         assertEquals(1, u.pset[0]);
         assertEquals(1, u.pset[1]);
-        assertEquals(2, u.pset[2]);        
+        assertEquals(2, u.pset[2]);
         assertEquals(3, u.pset[3]);
         assertEquals(4, u.pset[4]);
+        assertArrayEquals(new int[]{1, 2, 1, 1, 1}, u.getSizes());
 
         u.unionSet(1, 2);
         assertEquals(1, u.pset[0]);
         assertEquals(2, u.pset[1]);
-        assertEquals(2, u.pset[2]);        
+        assertEquals(2, u.pset[2]);
         assertEquals(3, u.pset[3]);
         assertEquals(4, u.pset[4]);
+        assertArrayEquals(new int[]{1, 2, 3, 1, 1}, u.getSizes());
 
         u.unionSet(3, 1);
         assertEquals(1, u.pset[0]);
         assertEquals(2, u.pset[1]);
-        assertEquals(2, u.pset[2]);        
+        assertEquals(2, u.pset[2]);
         assertEquals(2, u.pset[3]);
         assertEquals(4, u.pset[4]);
+        assertArrayEquals(new int[]{1, 2, 4, 1, 1}, u.getSizes());
+
 
         u.findSet(0);
         assertEquals(2, u.pset[0]);
         assertEquals(2, u.pset[1]);
-        assertEquals(2, u.pset[2]);        
+        assertEquals(2, u.pset[2]);
         assertEquals(2, u.pset[3]);
         assertEquals(4, u.pset[4]);
+        assertArrayEquals(new int[]{1, 2, 4, 1, 1}, u.getSizes());
+
 
         assertEquals(false, u.isSameSet(0, 4));
     }
