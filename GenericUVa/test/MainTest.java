c2a2a091c1c761b43bac840c0181de4a73a28dd1
Erwan Demairy <erwan.demairy@laposte.net>
2013-07-17 16:27:21 +0200

    - the tester scan the input*.txt files and compare the output with the result*.txt files.

--- GenericUVa/test/MainTest.java
+++ GenericUVa/test/MainTest.java
@@ -4,9 +4,16 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 import java.io.*;
 import java.net.URISyntaxException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.junit.AfterClass;
 import static org.junit.Assert.assertEquals;
 import org.junit.BeforeClass;
@@ -32,13 +39,30 @@ public class MainTest {
     @Test
     public void testInput1() throws FileNotFoundException, IOException, URISyntaxException {
         File tempOutput = File.createTempFile("test", "txt");
-        File expectedResult = new File(MainTest.class.getResource("/result.txt").toURI());
-        System.setIn(MainTest.class.getResourceAsStream("/input.txt"));
-        System.setOut(new PrintStream(tempOutput));
-        Main.main(new String[0]);
-        String result = getFileContent(tempOutput);
-        assertEquals(getFileContent(expectedResult), result);
-        System.err.println(result);
+        Path dirPath = FileSystems.getDefault().getPath("./test");
+        DirectoryStream<Path> fs = Files.newDirectoryStream(dirPath.toAbsolutePath(), "input*.txt");
+        Pattern testPattern = Pattern.compile("(.*)input(.*).txt");
+        for (Path path : fs) {
+            Matcher matcher = testPattern.matcher(path.toString());
+            if (matcher.matches()) {
+                System.err.printf("----- Testing with file %s -----\n", path.toString());
+                
+                StringBuilder testFileName = new StringBuilder();
+                testFileName.append(matcher.group(1)).append("result").append(matcher.group(2)).append(".txt");
+                System.err.printf("----- Searching for result file %s -----\n", testFileName);
+                
+                
+                File expectedResult = new File(testFileName.toString());
+                System.setIn(new FileInputStream(path.toFile()));
+                System.setOut(new PrintStream(tempOutput));
+                Main.main(new String[0]);
+                String result = getFileContent(tempOutput);
+                assertEquals(getFileContent(expectedResult), result);
+                System.err.println(result);
+                System.err.println(tempOutput.delete());
+                tempOutput = File.createTempFile("test", "txt");
+            }
+        }
     }
 
     private String getFileContent(final File f) throws FileNotFoundException, IOException {
