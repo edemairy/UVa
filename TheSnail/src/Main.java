4d7b86cc7d81659f24d980f98e183153cdbf392d
Erwan Demairy <erwan.demairy@inria.fr>
2014-11-06 23:34:54 +0100

    WA

---
+++ TheSnail/src/Main.java
@@ -0,0 +1,141 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Formatter;
+import java.util.Locale;
+
+/**
+ *
+ * @author edemairy
+ */
+public class Main {
+
+	private int nbTC;
+	private StringBuilder result = new StringBuilder();
+
+	private static class EndException extends RuntimeException {
+	}
+
+	public void run() throws IOException {
+		//        Scanner scanner = new Scanner(System.in);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		nbTC = 1;//readInt(reader);
+//         nbTC = Integer.MAX_VALUE;
+//        scanner.nextLine();
+		try {
+			for (int tc = 1; tc <= nbTC; ++tc) {
+				result.append(oneTestCase(reader));
+//				result.append('\n');
+			}
+		} catch (EndException e) {
+		}
+		System.out.print(result);
+	}
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) throws IOException {
+		Main main = new Main();
+		main.run();
+	}
+
+	private StringBuilder oneTestCase(BufferedReader reader) throws IOException {
+		Formatter formatter = new Formatter(Locale.ENGLISH);
+		StringBuilder output = new StringBuilder();
+//        for (int i = 0; i < 5; ++i) {
+//            formatter.format("%3d", n[i]);
+//        }
+		mainloop:
+		while (true) {
+			int h = readInt(reader);
+			if (h != 0) {
+				int u = readInt(reader);
+				int d = readInt(reader);
+				int f = readInt(reader);
+
+				h *= 100;
+				u *= 100;
+				d *= 100;
+				f = u * f / 100;
+				int day = 0;
+				int cu = u;
+				int ch = 0;
+				boolean reached = false;
+				while (ch >= 0 && !reached) {
+					ch += cu;
+					reached |= (ch > h);
+					ch -= d;
+					cu -= f;
+					day ++;
+				}
+				if (reached){
+					formatter.format("success on day %d\n", day);
+				} else {
+					formatter.format("failure on day %d\n", day);
+				}
+			} else {
+				break mainloop;
+			}
+		}
+
+		output.append(formatter.out());
+		return output;
+	}
+
+	private int readInt(BufferedReader reader) throws IOException {
+		int r = 0;
+		boolean positive = true;
+		char currentChar = (char) reader.read();
+
+		while ((currentChar == ' ') || (currentChar == '\n')) {
+			currentChar = (char) reader.read();
+		}
+		if (currentChar == (char) -1) {
+			throw new IOException("end of stream");
+		}
+		if (currentChar == '-') {
+			positive = false;
+			currentChar = (char) reader.read();
+		}
+		while ((currentChar >= '0') && (currentChar <= '9')) {
+			r = r * 10 + currentChar - '0';
+			currentChar = (char) reader.read();
+		}
+		if (positive) {
+			return r;
+		} else {
+			return -r;
+		}
+	}
+
+	private long readLong(BufferedReader reader) throws IOException {
+		long r = 0;
+		boolean positive = true;
+		char currentChar = (char) reader.read();
+
+		while ((currentChar == ' ') || (currentChar == '\n')) {
+			currentChar = (char) reader.read();
+		}
+		if (currentChar == (char) -1) {
+			throw new IOException("end of stream");
+		}
+		if (currentChar == '-') {
+			positive = false;
+			currentChar = (char) reader.read();
+		}
+		while ((currentChar >= '0') && (currentChar <= '9')) {
+			r = r * 10 + currentChar - '0';
+			currentChar = (char) reader.read();
+		}
+		if (positive) {
+			return r;
+		} else {
+			return -r;
+		}
+	}
+
+	private char readChar(BufferedReader reader) throws IOException {
+		return (char) reader.read();
+	}
+}
