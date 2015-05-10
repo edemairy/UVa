6236612fb616148d80c23b42e14cadb8308d4865
Erwan Demairy <erwan.demairy@inria.fr>
2015-05-10 10:53:33 +0200

    Accepted.

---
+++ MotherBear/src/Main.java
@@ -0,0 +1,142 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Formatter;
+import java.util.Locale;
+
+/**
+ *
+ * @author edemairy
+ */
+public class Main {
+
+	static public class Palyndrome {
+
+		public static boolean isPalyndrome(String s) {
+			int n = s.length();
+			if (n == 0) {
+				return true;
+			}
+			for (int i = 0; i <= s.length() / 2; i++) {
+				if (s.charAt(i) != s.charAt(n - i - 1)) {
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+
+	private int nbTC;
+	private StringBuilder result = new StringBuilder();
+
+	private static class EndException extends RuntimeException {
+	}
+
+	public void run() throws IOException {
+		//        Scanner scanner = new Scanner(System.in);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        nbTC = readInt(reader);
+		nbTC = Integer.MAX_VALUE;
+//        scanner.nextLine();
+		try {
+			for (int tc = 1; tc <= nbTC; ++tc) {
+				result.append(oneTestCase(reader));
+				result.append('\n');
+			}
+		} catch (EndException e) {
+		}
+		System.out.print(result);
+	}
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) throws IOException {
+		Main main = new Main();
+		main.run();
+	}
+
+	private StringBuilder oneTestCase(BufferedReader reader) throws IOException {
+		Formatter formatter = new Formatter(Locale.ENGLISH);
+		StringBuilder output = new StringBuilder();
+//        for (int i = 0; i < 5; ++i) {
+//            formatter.format("%3d", n[i]);
+//        }
+
+		String line = reader.readLine();
+		if (line.equals("DONE")) {
+			throw new EndException();
+		}
+		line = line.toUpperCase();
+		StringBuilder sb = new StringBuilder(line.length());
+		for (char c : line.toCharArray()) {
+			if (c >= 'A' && c <= 'Z') {
+				sb.append(c);
+			}
+		}
+		if (Palyndrome.isPalyndrome(sb.toString())) {
+			formatter.format("You won't be eaten!");
+		} else {
+			formatter.format("Uh oh..");
+		}
+		output.append(formatter.out());
+		return output;
+	}
+
+	private int readInt(BufferedReader reader) throws IOException {
+		int r = 0;
+		boolean positive = true;
+		char currentChar = (char) reader.read();
+
+		while ((currentChar == ' ') || (currentChar == '\n')) {
+			currentChar = (char) reader.read();
+		}
+		if (currentChar == (char) -1) {
+			throw new IOException("end of stream");
+		}
+		if (currentChar == '-') {
+			positive = false;
+			currentChar = (char) reader.read();
+		}
+		while ((currentChar >= '0') && (currentChar <= '9')) {
+			r = r * 10 + currentChar - '0';
+			currentChar = (char) reader.read();
+		}
+		if (positive) {
+			return r;
+		} else {
+			return -r;
+		}
+	}
+
+	private long readLong(BufferedReader reader) throws IOException {
+		long r = 0;
+		boolean positive = true;
+		char currentChar = (char) reader.read();
+
+		while ((currentChar == ' ') || (currentChar == '\n')) {
+			currentChar = (char) reader.read();
+		}
+		if (currentChar == (char) -1) {
+			throw new IOException("end of stream");
+		}
+		if (currentChar == '-') {
+			positive = false;
+			currentChar = (char) reader.read();
+		}
+		while ((currentChar >= '0') && (currentChar <= '9')) {
+			r = r * 10 + currentChar - '0';
+			currentChar = (char) reader.read();
+		}
+		if (positive) {
+			return r;
+		} else {
+			return -r;
+		}
+	}
+
+	private char readChar(BufferedReader reader) throws IOException {
+		return (char) reader.read();
+	}
+}
