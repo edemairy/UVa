9f4ea49e83d2aaa5f5920710e54e30f6dd3bab06
Erwan Demairy <erwan.demairy@inria.fr>
2015-06-03 16:12:51 +0200

    Accepted.

---
+++ CD/src/Main.java
@@ -0,0 +1,126 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.BitSet;
+import java.util.Formatter;
+import java.util.Locale;
+
+/**
+ *
+ * @author edemairy
+ */
+public class Main {
+
+    private int nbTC;
+    private StringBuilder result = new StringBuilder();
+
+    private static class EndException extends RuntimeException {
+    }
+
+    public void run() throws IOException {
+        //        Scanner scanner = new Scanner(System.in);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        nbTC = readInt(reader);
+         nbTC = Integer.MAX_VALUE;
+//        scanner.nextLine();
+        try {
+            for (int tc = 1; tc <= nbTC; ++tc) {
+                result.append(oneTestCase(reader));
+                result.append('\n');
+            }
+        } catch (EndException e) {
+        }
+        System.out.print(result);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.run();
+    }
+
+    private StringBuilder oneTestCase(BufferedReader reader) throws IOException {
+        Formatter formatter = new Formatter(Locale.ENGLISH);
+        StringBuilder output = new StringBuilder();
+//        for (int i = 0; i < 5; ++i) {
+//            formatter.format("%3d", n[i]);
+//        }
+	int nb1 = readInt(reader);
+	int nb2 = readInt(reader);
+	if (nb1 == 0 && nb2 == 0){
+		throw new EndException();
+	}
+	int res = 0;
+	BitSet own = new BitSet();
+	for (int i=0; i<nb1; i++) {
+		own.set(readInt(reader));
+	}
+	for (int i=0; i<nb2; i++) {
+		if (own.get(readInt(reader))) {
+			res++;
+		}	
+	}
+	formatter.format("%d", res);
+        output.append(formatter.out());
+        return output;
+    }
+
+    private int readInt(BufferedReader reader) throws IOException {
+        int r = 0;
+        boolean positive = true;
+        char currentChar = (char) reader.read();
+
+        while ((currentChar == ' ') || (currentChar == '\n')) {
+            currentChar = (char) reader.read();
+        }
+        if (currentChar == (char) -1) {
+            throw new IOException("end of stream");
+        }
+        if (currentChar == '-') {
+            positive = false;
+            currentChar = (char) reader.read();
+        }
+        while ((currentChar >= '0') && (currentChar <= '9')) {
+            r = r * 10 + currentChar - '0';
+            currentChar = (char) reader.read();
+        }
+        if (positive) {
+            return r;
+        } else {
+            return -r;
+        }
+    }
+
+    private long readLong(BufferedReader reader) throws IOException {
+        long r = 0;
+        boolean positive = true;
+        char currentChar = (char) reader.read();
+
+        while ((currentChar == ' ') || (currentChar == '\n')) {
+            currentChar = (char) reader.read();
+        }
+        if (currentChar == (char) -1) {
+            throw new IOException("end of stream");
+        }
+        if (currentChar == '-') {
+            positive = false;
+            currentChar = (char) reader.read();
+        }
+        while ((currentChar >= '0') && (currentChar <= '9')) {
+            r = r * 10 + currentChar - '0';
+            currentChar = (char) reader.read();
+        }
+        if (positive) {
+            return r;
+        } else {
+            return -r;
+        }
+    }
+
+    private char readChar(BufferedReader reader) throws IOException {
+        return (char) reader.read();
+    }
+}
